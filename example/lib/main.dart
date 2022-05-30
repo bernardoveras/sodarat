93941081dab8749ebb8e684a5f5be14190c17ae7
Bernardo Veras <bernardo@vvssistemas.com.br>
2022-05-29 21:09:06 -0300

    Refatoração

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -42,32 +42,37 @@ class HomePage extends HookWidget {
     }
 
     return Scaffold(
-      backgroundColor: $adaptive_color.background(context),
-      // appBar: AppBar(
-      //   title: const Text('Sodarat'),
-      //   backgroundColor: $adaptive_color.primary(),
-      //   foregroundColor: $adaptive_color.on_primary(),
-      // ),
       appBar: const HomeAppBar(username: 'Bernardo Veras'),
+      backgroundColor: $adaptive_color.background(context),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const SodaratCircularSpinner(),
-            const SizedBox(height: $spacing_4),
-            const SodaratDotSpinner(),
-            const SizedBox(height: $spacing_4),
-            ResultIcon.error(),
-            const SizedBox(height: $spacing_4),
+            Wrap(
+              spacing: $spacing_4,
+              children: const [
+                SodaratCircularSpinner(),
+                SodaratDotSpinner(),
+              ],
+            ),
+            const SizedBox(height: $spacing_5),
+            Wrap(
+              spacing: $spacing_4,
+              children: [
+                ResultIcon.error(),
+                ResultIcon.success(),
+              ],
+            ),
+            const SizedBox(height: $spacing_5),
             ElevatedButton(
               child: const Text('Abrir loading'),
               onPressed: () {
                 abrirLoading();
               },
             ),
-            const SizedBox(height: $spacing_4),
+            const SizedBox(height: $spacing_5),
             Wrap(
               spacing: $spacing_4,
               children: [
